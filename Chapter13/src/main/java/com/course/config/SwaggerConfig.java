7519a1c897ae74d9ebe2715e12a1d4065ffdb065
zhaokangping <1728573563@qq.com>
2019-10-08 10:17:23 +0800

    add Chapter13

---
+++ Chapter13/src/main/java/com/course/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.course.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .pathMapping("/")
+                .select()
+                .paths(PathSelectors.regex("/.*"))  //正则匹配
+                .build();
+
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("UserManager service API")
+                .contact(new Contact("test","","123@qq.com"))
+                .description("this is UserManager service API")
+                .version("1.0")
+                .build();
+
+    }
+
+
+}
